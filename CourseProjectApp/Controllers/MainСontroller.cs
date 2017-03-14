e97bdada1f11965133a49bbbc403fa59a1f77580
Anton Borodulin <aborod@softserveinc.com>
2017-03-14 14:56:54 +0200

    Add project files.

---
+++ CourseProjectApp/Controllers/MainСontroller.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CourseProjectApp.Controllers
+{
+    [Route("Main")]
+    public class MainСontroller : Controller
+    {
+        [Route("Index")]
+        public IActionResult Index()
+        {
+            ViewBag.value = "My First MVC Route!!!";
+            return View();
+        }
+    }
+}
